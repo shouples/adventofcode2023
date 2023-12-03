9519c1d5eeda9546c641af5e5d9b15b088613768
Dave Shoup <dave.shoup@gmail.com>
2023-12-03 16:59:06 -0500

    add template for future puzzles

---
+++ py/solutions/_template.py
@@ -0,0 +1,24 @@
+from utils import timer
+
+
+@timer
+def solve(inputs: list[str]) -> int:
+    """High-level solution logic to call additional functions as needed."""
+    pass
+
+
+@timer
+def load_input() -> list[str]:
+    """Read from an input file, handle any preprocessing, return a list of strings."""
+    pass
+
+
+@timer
+def run() -> None:
+    inputs: list[str] = load_input()
+    solution = solve(inputs)
+    print(f"{solution=}")
+
+
+if __name__ == "__main__":
+    run()

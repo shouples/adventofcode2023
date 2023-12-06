2c29acdbf03a1b8dbb44ee5f7038d5164f7abe97
Dave Shoup <dave.shoup@gmail.com>
2023-12-06 16:30:06 -0500

    add rust template

---
+++ rs/src/bin/_template.rs
@@ -0,0 +1,23 @@
+use adventofcode2023::timer;
+
+fn solve(input: String) -> i32 {
+    // High-level solution logic to call additional functions as needed.
+    1
+}
+
+fn load_input() -> String {
+    // Read from an input file and handle any preprocessing.
+    let input_str = std::fs::read_to_string("../inputs/1.txt").unwrap();
+    // add any preprocessing here
+    input_str
+}
+
+fn run() {
+    let input: String = timer! { "load_input", load_input() };
+    let solution: i32 = timer! { "solve", solve(input) };
+    dbg!(solution);
+}
+
+fn main() {
+    timer! { "run", run() };
+}

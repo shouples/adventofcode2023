e8c370f4774133dba3b4a5dd8a38ee142493990f
Dave Shoup <dave.shoup@gmail.com>
2023-12-06 19:33:43 -0500

    add day 2 rust tests

--- rs/src/bin/2-2.rs
+++ rs/src/bin/2-2.rs
@@ -61,3 +61,26 @@ fn run() {
 fn main() {
     timer! { "run", run() };
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_2() {
+        let inputs: Vec<String> = vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]
+        .iter()
+        .map(|&s| s.to_owned())
+        .collect();
+
+        let solution = solve(inputs);
+
+        assert_eq!(solution, 2286);
+    }
+}

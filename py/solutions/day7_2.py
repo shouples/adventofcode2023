242a595b6718eddf4e97d79f193f32cb84c28821
Dave Shoup <dave.shoup@gmail.com>
2023-12-08 14:48:18 -0500

    py day 7 part 2 updates

--- py/solutions/day7_2.py
+++ py/solutions/day7_2.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 from solutions.utils import timer
 
-CARD_RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+CARD_RANKS_WITH_JOKERS = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
 
 
 class Card:
@@ -23,19 +23,20 @@ class Card:
 
     @property
     def score(self) -> int:
-        return CARD_RANKS.index(self.value)
+        return CARD_RANKS_WITH_JOKERS.index(self.value)
 
 
 HAND_RANKS = ["HC", "1P", "2P", "3K", "FH", "4K", "5K"]
 
 
-class CardHand:
+class CardHandWithJokers:
     def __init__(self, hand_data: str):
         self.raw_cards, bid = hand_data.split()
         self.bid = int(bid)
         self.cards: list[Card] = [Card(value) for value in self.raw_cards]
 
         self.card_counts: dict[str, int] = Counter(card.value for card in self.cards)
+        self.adjust_counts_for_jokers()
 
         self.one_pair = [count for count in self.card_counts.values() if count == 2]
         self.two_pair = len(self.one_pair) == 2
@@ -43,6 +44,29 @@ class CardHand:
         self.full_house = self.one_pair and self.three_of_a_kind
         self.four_of_a_kind = [count for count in self.card_counts.values() if count == 4]
         self.five_of_a_kind = [count for count in self.card_counts.values() if count == 5]
+
+    def adjust_counts_for_jokers(self):
+        self.joker_count = self.card_counts.pop("J", 0)
+        if self.joker_count == 0:
+            # no jokers, no adjustments
+            return
+
+        if self.joker_count == 5:
+            # all jokers, max score
+            self.card_counts = {"A": 5}
+            return
+
+        top_count = max(self.card_counts.values())
+        top_count_cards = [card for card, count in self.card_counts.items() if count == top_count]
+        if len(top_count_cards) == 1:
+            # one non-Joker card is the top count, add jokers to it
+            self.card_counts[top_count_cards[0]] += self.joker_count
+            return
+
+        # multiple top count cards, add jokers to the highest value card (2P)
+        unique_cards = set(top_count_cards)
+        higher_card = max(unique_cards, key=lambda x: CARD_RANKS_WITH_JOKERS.index(x))
+        self.card_counts[higher_card] += self.joker_count
 
     @property
     def rank(self) -> str:
@@ -74,16 +98,16 @@ class CardHand:
     def __repr__(self) -> str:
         return f"<CardHand {self.raw_cards}: {self.rank} (score={self.rank_score})>"
 
-    def __eq__(self, other: "CardHand") -> bool:
+    def __eq__(self, other: "CardHandWithJokers") -> bool:
         return self.cards == other.cards
 
-    def __gt__(self, other: "CardHand") -> bool:
+    def __gt__(self, other: "CardHandWithJokers") -> bool:
         for card, other_card in zip(self.cards, other.cards):
             if card == other_card:
                 continue
             return card > other_card
 
-    def __lt__(self, other: "CardHand") -> bool:
+    def __lt__(self, other: "CardHandWithJokers") -> bool:
         for card, other_card in zip(self.cards, other.cards):
             if card == other_card:
                 continue
@@ -102,7 +126,9 @@ def calculate_winnings(hands) -> int:
 @timer
 def solve(inputs: list[str]) -> int:
     """High-level solution logic to call additional functions as needed."""
-    hands = [CardHand(hand_data) for hand_data in inputs]
+    hands = sorted(
+        [CardHandWithJokers(hand_data) for hand_data in inputs], key=lambda x: x.rank_score
+    )
     return calculate_winnings(hands)
 
 
